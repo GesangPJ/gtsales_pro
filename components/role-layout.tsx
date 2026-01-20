ec975bae78bf55f548e8cc68ae489de78f1faa9d
G.PAUDRA <g.paudra@gmail.com>
2026-01-20 22:58:35 +0700

    sidebar kondisional

---
+++ components/role-layout.tsx
@@ -0,0 +1,50 @@
+
+// Komponen menentukan sidebar berdasarkan tipe akun
+
+import React, { ReactNode } from "react"
+import { auth } from '@/lib/auth'
+import {
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar"
+import { SiteHeader } from "@/components/site-header"
+import { AdminSidebar } from './admin-sidebar'
+import { UserSidebar } from './user-sidebar'
+import { OwnerSidebar } from './owner-sidebar'
+
+interface Props {
+    children?: ReactNode
+}
+
+export async function RoleLayout({children}: Props) {
+  const session = await auth.api.getSession()
+  const tipe = session?.user?.tipe
+
+  return (
+    <SidebarProvider
+    style={{
+        "--sidebar-width": "calc(var(--spacing) * 72)",
+        "--header-height": "calc(var(--spacing) * 12)",
+      } as React.CSSProperties}
+    >
+      {tipe === "admin" ? (
+      <AdminSidebar />
+    ) : tipe === "owner" ? (
+      <OwnerSidebar />
+    ) : (
+      <UserSidebar />
+    )}
+      <SidebarInset>
+        <SiteHeader />
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            {children}
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
+
+
+
