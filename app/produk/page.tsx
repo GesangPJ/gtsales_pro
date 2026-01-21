d01c2cd3accec0d697aca65ae95183f1b1d3ece2
G.PAUDRA <g.paudra@gmail.com>
2026-01-21 21:33:16 +0700

    halaman daftar produk

--- app/produk/page.tsx
+++ app/produk/page.tsx
@@ -1,10 +1,16 @@
 // Halaman Daftar Produk
 
+import TabelProduk from "./tabel-produk"
+
+
 export default function HalamanProduk(){
     return(
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
             <div className="px-4 lg:px-6">
-                <h1 className="font-bold text-xl">Halaman Produk</h1>
+                <h1 className="font-bold text-xl">Daftar Produk</h1>
+            </div>
+            <div className="px-4 lg:px-6">
+                <TabelProduk/>
             </div>
         </div>
     )
