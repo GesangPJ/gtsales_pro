511098cd4f49a491f8d2ca4954f06eadb11daf56
G.PAUDRA <g.paudra@gmail.com>
2026-01-21 18:13:37 +0700

    halaman tambah produk

--- app/produk/tambah-produk/page.tsx
+++ app/produk/tambah-produk/page.tsx
@@ -1,12 +1,17 @@
 
 // Halaman Tambah Produk
 
+import FormTambahProduk from "./form-tambahproduk"
+
 export default function HalamanTambahProduk(){
 
     return(
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
             <div className="px-4 lg:px-6">
                 <h1 className="font-bold text-xl">Tambah Produk</h1>
+            </div>
+            <div className="px-4 lg:px-6">
+                <FormTambahProduk/>
             </div>
         </div>
     )
