945dd198d1d6dbad6356de427b76f72a1b4ed1c3
G.PAUDRA <g.paudra@gmail.com>
2026-01-21 13:43:17 +0700

    tambah halaman

---
+++ app/pembelian/page.tsx
@@ -0,0 +1,16 @@
+// Halaman Pembelian (Request Pembelian)
+
+import FormPembelian from "./form-pembelian"
+
+export default function HalamanBuatPembelian() {
+  return (      
+  <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+      <div className="px-4 lg:px-6">
+          <h1 className="text-2xl font-bold">Pembelian Barang </h1>
+      </div>
+    <div className="px-4 lg:px-6">
+      {/* <FormPembelian/> */}
+    </div>  
+  </div>
+  )
+}
