7581ebd8fb8b558fcc9a71a380bc0eae807621fb
G.PAUDRA <g.paudra@gmail.com>
2026-01-21 13:55:23 +0700

    buat halaman daftar produk

---
+++ app/produk/page.tsx
@@ -0,0 +1,14 @@
+// Halaman Daftar Produk
+
+export default function HalamanProduk(){
+    return(
+        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+            <div className="px-4 lg:px-6">
+                <h1 className="font-bold text-xl">Halaman Produk</h1>
+            </div>
+        </div>
+    )
+}
+
+
+
