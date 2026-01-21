f2233309197ed00d1415daf09dcd4b1b48dd33f0
G.PAUDRA <g.paudra@gmail.com>
2026-01-21 17:09:34 +0700

    halaman kategori produk

--- app/kategori/page.tsx
+++ app/kategori/page.tsx
@@ -1,12 +1,15 @@
 // Halaman Kategori
 
-
+import TabelKategori from "./tabel-kategori"
 
 export default function HalamanKategori(){
     return(
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
             <div className="px-4 lg:px-6">
-                <h1 className="font-bold text-xl">Halaman Kategori Produk</h1>
+                <h1 className="font-bold text-xl">Kategori Produk</h1>
+            </div>
+            <div className="px-4 lg:px-6 max-w-120">
+                <TabelKategori />
             </div>
         </div>
     )
