d6f1ecb7d859b6dfee36d63389796893aced4d4b
G.PAUDRA <g.paudra@gmail.com>
2026-01-21 12:57:58 +0700

    remove captcha

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,7 +2,6 @@ import { betterAuth } from "better-auth"
 import { prismaAdapter } from "better-auth/adapters/prisma"
 import {prisma} from "@/lib/prisma"
 import { jwt } from "better-auth/plugins"
-import { captcha } from "better-auth/plugins"
 import { nextCookies } from "better-auth/next-js"
 
 
@@ -26,10 +25,6 @@ export const auth = betterAuth({
   },
   plugins: [ 
         jwt(),
-        captcha({ 
-            provider: "cloudflare-turnstile",
-            secretKey: process.env.TURNSTILE_SECRET_KEY!, 
-        }),
         nextCookies(),
     ] 
 })
