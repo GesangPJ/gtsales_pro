5ddd97a7527d8b358b059ff7cb0de3fa53ef3b43
G.PAUDRA <g.paudra@gmail.com>
2026-01-21 12:57:23 +0700

    script seed akun admin

---
+++ scripts/buat-akun-admin.ts
@@ -0,0 +1,38 @@
+// Script membuat akun admin
+
+import { auth } from "@/lib/auth"
+
+async function registerAdmin(){
+    const ADMIN_EMAIL = "admin@email.com"
+    const ADMIN_PASSWORD = "admin123456"
+    const ADMIN_NAME = "Admin test 01"
+
+    try{
+        
+        const result = await auth.api.signUpEmail({
+            body:{
+                name: ADMIN_NAME,
+                email: ADMIN_EMAIL,
+                password: ADMIN_PASSWORD,
+                tipe:"admin",
+            },
+        })
+
+        const newAdmin = result.user
+
+        console.log("Admin berhasil dibuat")
+        console.log(`📧 Email:    ${newAdmin?.email}`)
+        console.log(`👤 Nama:     ${newAdmin?.name}`)
+        console.log(`🎭 Tipe:     ${newAdmin?.tipe}`)
+        console.log(`📝 Password: ${ADMIN_PASSWORD}`)
+
+
+    }catch(error: any){
+        console.error("❌ ERROR MEMBUAT ADMIN:");
+        console.error(error.message);
+    }
+}
+
+registerAdmin()
+
+
