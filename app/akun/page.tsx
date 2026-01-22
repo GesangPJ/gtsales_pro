d7e6b8d39299c4971e27168ede353fe70840bc8d
G.PAUDRA <g.paudra@gmail.com>
2026-01-22 16:17:19 +0700

    buat halaman tabel daftar akun

---
+++ app/akun/page.tsx
@@ -0,0 +1,19 @@
+
+// Halaman tabel daftar akun
+
+import TabelAkun from "./tabel-akun"
+
+export default function HalamanAkun(){
+
+    return(
+        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+            <div className="px-4 lg:px-6">
+                <h1 className="text-lg font-bold">Tabel Daftar Akun</h1>
+            </div>
+            <div className="px-4 lg:px-6">
+                <TabelAkun/>
+            </div>
+        </div>
+    )
+}
+
