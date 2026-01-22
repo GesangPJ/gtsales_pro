ae6f41797c0143af2fac971741d4d6c46d51a3c4
G.PAUDRA <g.paudra@gmail.com>
2026-01-22 13:08:48 +0700

    update sidebar akun

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -87,7 +87,7 @@ export function NavUser({ user }: NavUserProps) {
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
+                  <AvatarImage src='/avatars/shadcn.jpg' alt={user.name} />
                   <AvatarFallback className="rounded-lg">AD</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
