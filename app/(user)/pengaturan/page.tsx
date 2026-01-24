a482aa63391524e5e27427cabadb1d9195ea396f
G.PAUDRA <g.paudra@gmail.com>
2026-01-24 13:57:01 +0700

    update style halaman pengaturan akun

--- app/(user)/pengaturan/page.tsx
+++ app/(user)/pengaturan/page.tsx
@@ -8,7 +8,7 @@ export default function HalamanPengaturan(){
 
     return(
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-            <div className="px-4 lg:px-6">
+            <div className="px-4 lg:px-4">
                 <div className=" grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
                     <KomponenAkun/>
                     <GantiPassword/>
