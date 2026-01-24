90bb53d47e59202216b89d4fb6fb91a96eca5622
G.PAUDRA <g.paudra@gmail.com>
2026-01-24 21:09:47 +0700

    update title style

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { LoginForm } from "@/components/signin-form"
-import { ShoppingBag } from "lucide-react"
 import { AppInfo } from "@/components/app-info"
+import { ShoppingBag } from "lucide-react"
 import { ModeToggle } from "@/components/mode-toggle"
 
 export default function Page() {
@@ -8,9 +8,7 @@ return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
         <div  className="flex items-center gap-2 self-center font-medium">
-          <div className="bg-white text-primary-foreground flex size-6 items-center justify-center rounded-md">
-            <ShoppingBag className="size-4" />
-          </div>
+          <ShoppingBag/>
           <h1 className="font-bold text-2xl">GTSALES <span className="font-bold text-yellow-600">PRO</span></h1><AppInfo/> <ModeToggle/>
         </div>
         <LoginForm />
