96b93baebd5b5bd93e7a8996da6154aebee644a6
G.PAUDRA <g.paudra@gmail.com>
2026-01-25 19:51:02 +0700

    halaman tabel distributor

---
+++ app/(admin)/distributor/page.tsx
@@ -0,0 +1,19 @@
+
+// Halaman Distributor
+
+import TabelDistributor from "./tabel-distributor"
+
+export default function HalamanDistributor(){
+
+    return(
+        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+            <div className="px-4 lg:px-6">
+                <TabelDistributor/>
+            </div>
+
+        </div>
+    )
+}
+
+
+
