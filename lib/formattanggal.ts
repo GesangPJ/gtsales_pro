740aea1263a99da95910ec39b365da4ec7856a2a
G.PAUDRA <g.paudra@gmail.com>
2026-01-26 19:31:51 +0700

    tambah function jam

--- lib/formattanggal.ts
+++ lib/formattanggal.ts
@@ -18,5 +18,24 @@ export const formatTanggal = (isoString: string | Date | null): string => {
   }
 }
 
+export const formatTanggalJam = (isoString: string | Date | null): string => {
+  if (!isoString) return '-'
+  
+  try {
+    const date = typeof isoString === 'string' 
+      ? new Date(isoString.replace(/\+00:00$/, ''))
+      : isoString
+    
+    return new Intl.DateTimeFormat('id-ID', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit'
+    }).format(date)
+  } catch {
+    return '-'
+  }
+}
 
 
