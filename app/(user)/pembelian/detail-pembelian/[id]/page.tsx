05128016fb5a96ea4018ac71f44018039857a258
G.PAUDRA <g.paudra@gmail.com>
2026-01-26 21:37:36 +0700

    ganti style tombol kembali

--- app/(user)/pembelian/detail-pembelian/[id]/page.tsx
+++ app/(user)/pembelian/detail-pembelian/[id]/page.tsx
@@ -89,7 +89,7 @@ export default async function HalamanDetailPembelian({params}: DetailProps){
                   
                 </div>
 
-                <Button asChild size="lg" variant="default" className="mt-3">
+                <Button asChild size="lg"  className="mt-3 bg-sky-600/10 text-sky-600 hover:bg-sky-600/20 focus-visible:ring-sky-600/20 dark:bg-sky-400/10 dark:text-sky-400 dark:hover:bg-sky-400/20 dark:focus-visible:ring-sky-400/40">
                   <Link href='/pembelian/data-pembelian'>
                   <ArrowLeft/>
                   Kembali</Link>
