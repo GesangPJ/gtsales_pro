8b5a764766481125b64526421434766a77bd4d40
G.PAUDRA <g.paudra@gmail.com>
2026-01-27 13:41:09 +0700

    fix halaman pembelian produk

--- app/(user)/pembelian/page.tsx
+++ app/(user)/pembelian/page.tsx
@@ -6,7 +6,7 @@ export default function HalamanBuatPembelian() {
   return (      
   <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
       <div className="px-4 lg:px-6">
-          <h1 className="text-2xl font-bold">Pembelian Barang </h1>
+          <h1 className="text-2xl font-bold">Pembelian Produk</h1>
       </div>
     <div className="px-4 lg:px-6">
       <FormPembelian/>
