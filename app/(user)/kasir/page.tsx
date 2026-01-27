47e045bc860f168eca4f893ce6f72ab1ac5ff836
G.PAUDRA <g.paudra@gmail.com>
2026-01-27 13:42:11 +0700

    halaman kasir / POS

---
+++ app/(user)/kasir/page.tsx
@@ -0,0 +1,17 @@
+
+// Halaman POS / KASIR
+
+import FormKasir from "./kasir"
+
+export default function HalamanKasir(){
+
+    return(
+        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+            <div className="px-4 lg:px-6">
+               <FormKasir/>
+            </div>
+        </div>
+    )
+}
+
+
