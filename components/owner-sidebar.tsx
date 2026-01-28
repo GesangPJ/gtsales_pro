8ce6f613a904124f0fe3db46fe821bf8d3e60075
G.PAUDRA <g.paudra@gmail.com>
2026-01-28 20:27:44 +0700

    fix sidebar owner

--- components/owner-sidebar.tsx
+++ components/owner-sidebar.tsx
@@ -100,7 +100,7 @@ const data = {
       icon: IconTableFilled,
     },
     {
-      title: "Pembelian Barang",
+      title: "Pembelian Produk",
       url: "/pembelian",
       icon: IconTruckLoading,
     },
@@ -200,7 +200,6 @@ export function OwnerSidebar({user}: OwnerSidebarProps ) {
         <SidebarSeparator />
         <NavMain items={data.navMain} />
         <SidebarSeparator />
-        {/* <span className="px-2">Barang</span> */}
         <NavSecondary items={data.navTambah} className="mt-10px" />
         <SidebarSeparator />
         <NavSecondary items={data.navVendor} className="mt-10px" />
