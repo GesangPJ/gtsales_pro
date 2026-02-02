37454f2ffb46c2a8b047f2831f60766012c401c4
G.PAUDRA <g.paudra@gmail.com>
2026-02-02 16:05:18 +0700

    halaman laporan penjualan

---
+++ app/(owner)/laporan-penjualan/page.tsx
@@ -0,0 +1,15 @@
+// Halaman Laporan Penjualan
+
+export default function LaporanPenjualan(){
+
+    return(
+        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+            <div className="px-4 lg:px-6">
+                <h1 className="text-xl font-bold font-mono">Laporan Penjualan</h1>
+
+            </div>
+            
+        </div>
+    )
+}
+
